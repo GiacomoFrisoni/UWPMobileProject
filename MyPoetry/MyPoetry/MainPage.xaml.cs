207ad94d3aca56e2a2a1d68b4204e82c54d9d05a
Martinocom <martinoqom@gmail.com>
2017-05-22 20:54:51 +0200

    Adding first logic of menu

--- MyPoetry/MyPoetry/MainPage.xaml.cs
+++ MyPoetry/MyPoetry/MainPage.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿
+using MyPoetry.UserControls;
+using MyPoetry.UserControls.Menu;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,11 +29,74 @@ namespace MyPoetry
         public MainPage()
         {
             this.InitializeComponent();
+            GenerateMenu();
         }
 
-        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+
+        private void GenerateMenu()
         {
-            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+            List<GroupCollector<object>> menu = new List<GroupCollector<object>>();
+
+            GroupCollector<object> main = new GroupCollector<object>();
+            main.Key = "Principale";
+            main.Add(new MenuItem("Home", Symbol.Home));
+            main.Add(new MenuItem("Le mie creazioni", Symbol.Highlight));
+            main.Add(new MenuItem("Le mie plaquette", Symbol.Import));
+            menu.Add(main);
+
+            GroupCollector<object> create = new GroupCollector<object>();
+            create.Key = "Crea";
+            create.Add(new MenuItem("Nuova poesia", Symbol.Add));
+            create.Add(new MenuItem("Nuova plaquette", Symbol.Add));
+            menu.Add(create);
+
+            GroupCollector<object> explore = new GroupCollector<object>();
+            explore.Key = "Esplora";
+            explore.Add(new MenuItem("Dizionari", Symbol.Directions));
+            explore.Add(new MenuItem("Cerca rime", Symbol.DockRight));
+            menu.Add(explore);
+
+            var cvs = (CollectionViewSource)Resources["itemsViewSource"];
+            cvs.Source = menu;
+        }
+
+        /*
+        public List<GroupInfoList<object>> GetFriendsGrouped()
+        {
+            if (_friends == null)
+            {
+                _friends = await NetworkManager.GetFriends();
+            }
+
+            List<GroupInfoList<object>> friendsGrouped = new List<GroupInfoList<object>>();
+
+            var query = from friend in _friends
+                        orderby ((Friend)friend).first_name
+                        group friend by ((Friend)friend).first_name[0] into g
+                        select new { GroupName = g.Key, Items = g };
+            foreach (var g in query)
+            {
+                GroupInfoList<object> info = new GroupInfoList<object>();
+                info.Key = g.GroupName;
+                foreach (var friend in g.Items)
+                {
+                    info.Add(friend);
+                }
+                friendsGrouped.Add(info);
+            }
+            return friendsGrouped;
+        }*/
+
+        private void HamburgerMenuControl_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            /*var menuItem = e.ClickedItem as MenuItem;
+            ContentFrame.Navigate(menuItem.Page);
+            TxblTitle.Text = menuItem.Text;*/
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           /* HamburgerMenuControl.IsPaneOpen = !HamburgerMenuControl.IsPaneOpen;*/
         }
     }
 }
---
+++ MyPoetry/MyPoetry/UserControls/Menu/GroupCollector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPoetry.UserControls.Menu
+{
+    class GroupCollector<T> : List<object>
+    {   
+        public object Key { get; set; }
+        public new IEnumerator<object> GetEnumerator()
+        {
+            return base.GetEnumerator();
+        }
+        
+    }
+}
