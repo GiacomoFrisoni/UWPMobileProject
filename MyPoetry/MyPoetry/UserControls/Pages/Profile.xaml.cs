46eb81b5521721069f9ec27255cad40ea42eff7b
Marcin Pabich <martinoqom@gmail.com>
2017-07-03 17:59:58 +0200

    Profile implementation + Some general improvements

--- MyPoetry/MyPoetry/UserControls/Pages/Profile.xaml.cs
+++ MyPoetry/MyPoetry/UserControls/Pages/Profile.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyPoetry.Model;
+using MyPoetry.Utilities;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,5 +27,93 @@ namespace MyPoetry.UserControls.Pages
         }
 
         public CustomPage GetPage { get { return MainContent; } }
+
+
+        private async void LoadData()
+        {
+            ProgressBarVisible(true);
+
+            if (UserHandler.Instance.GetPoetries() == null)
+            {
+                List<Poetry> poetries = await App.MobileService.GetTable<Poetry>()
+                    .Where(p => p.UserId == UserHandler.Instance.GetUser().Id)
+                    .ToListAsync();
+                UserHandler.Instance.SetPoetries(poetries);
+            }
+
+
+            //Immagine
+            ImageBrush ib = new ImageBrush();
+            ib.ImageSource = await ImageHelper.ImageFromBytes(UserHandler.Instance.GetUser().Photo);
+            ImgImage.Source = ib.ImageSource;
+            ImgProfile.Source = ib.ImageSource;
+
+            //Nome e cognome
+            TxbUser.Text = UserHandler.Instance.GetUser().Name + " " + UserHandler.Instance.GetUser().Surname;
+            TxbName.Text = UserHandler.Instance.GetUser().Name;
+            TxbSurname.Text = UserHandler.Instance.GetUser().Surname;
+
+            //Gender
+            CmbGender.SelectedIndex = UserHandler.Instance.GetUser().Gender == "M" ? 0 : 1;
+
+            //Mail
+            TxbMail.Text = UserHandler.Instance.GetUser().Email;
+
+            //Data registrazione
+            TxbRegisterDate.Text = UserHandler.Instance.GetUser().RegistrationDate.ToString("dd/MM/yyyy");
+
+
+
+
+            //Poesie scritte
+            TxbPoetriesNumber.Text = UserHandler.Instance.GetPoetries().Count.ToString();
+
+            //Lunghezza complessiva (char)
+            TxbCharsNumber.Text = UserHandler.Instance.GetPoetries().Sum(poetry => poetry.CharactersNumber).ToString();
+
+            //Parole utilizzate
+            TxbWordsNumber.Text = UserHandler.Instance.GetPoetries().Sum(poetry => poetry.WordsNumber).ToString();
+
+            //Numero versi
+            TxbVersesNumber.Text = UserHandler.Instance.GetPoetries().Sum(poetry => poetry.VersesNumber).ToString();
+
+            //Quella più lunga
+            TxbLongest.Text = UserHandler.Instance.GetPoetries().OrderByDescending(poetry => poetry.CharactersNumber).First().Title;
+
+            //Quella più corta
+            TxbShortest.Text = UserHandler.Instance.GetPoetries().OrderBy(poetry => poetry.CharactersNumber).First().Title;
+
+            //Giorno di ispirazione
+            TxbInspiration.Text = "Smetti di scrivere...";
+
+            ProgressBarVisible(false);
+        }
+
+
+        private void ProgressBarVisible(bool visible)
+        {
+            ProgressRingProfile.Visibility = visible ? Visibility.Visible : Visibility.Collapsed;
+            ProgressRingProfile.IsActive = visible;
+        }
+
+        private void Pivot_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void BtnDeletePhoto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPhoto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ MyPoetry/MyPoetry/Utilities/ImageHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MyPoetry.Utilities
+{
+    class ImageHelper
+    {
+        static public async Task<BitmapImage> ImageFromBytes(Byte[] bytes)
+        {
+            BitmapImage image = new BitmapImage();
+            using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+            {
+                await stream.WriteAsync(bytes.AsBuffer());
+                stream.Seek(0);
+                await image.SetSourceAsync(stream);
+            }
+            return image;
+        }
+    }
+}
