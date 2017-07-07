33ccadc5fa8d31b7152ce18638845534cb7866a6
Marcin Pabich <martinoqom@gmail.com>
2017-07-07 14:51:48 +0200

    Redesign of Homepage - appbar, detailView, search

--- MyPoetry/MyPoetry/UserControls/Pages/Homepage.xaml.cs
+++ MyPoetry/MyPoetry/UserControls/Pages/Homepage.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -32,7 +33,7 @@ namespace MyPoetry.UserControls.Pages
         public CustomPage GetPage { get { return MainContent; } }
 
 
-
+        #region LoadingMasterDetail
         private void ProgressBarVisible(bool visible)
         {
             ProgressRingPoetries.Visibility = visible ? Visibility.Visible : Visibility.Collapsed;
@@ -52,22 +53,118 @@ namespace MyPoetry.UserControls.Pages
                 UserHandler.Instance.SetPoetries(poetries);
 
                 MasterDetailView.ItemsSource = poetries;
-                //PoetriesListView.ItemsSource = poetries;
 
-                //var groups = from c in UserHandler.Instance.GetPoetries() group c by c.CreationDate.Month;
-                //PoetriesListView.ItemsSource = groups;
             }
             else
             {
                 MasterDetailView.ItemsSource = null;
                 MasterDetailView.ItemsSource = UserHandler.Instance.GetPoetries();
-                //PoetriesListView.ItemsSource = null;
-                //var groups = from c in UserHandler.Instance.GetPoetries() group c by c.CreationDate.Month;
-                //PoetriesListView.ItemsSource = groups;
-                //PoetriesListView.ItemsSource = UserHandler.Instance.GetPoetries();
             }
 
             ProgressBarVisible(false);
+        }
+        #endregion
+
+
+        #region ButtonClick events
+        private void BtnShare_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDetails_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        #endregion
+
+        private void CmbOrderby_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ProgressBarVisible(true);
+            FlyOrderby.Hide();
+            MasterDetailView.ItemsSource = null;
+            MasterDetailView.ItemsSource = GetPoetriesWithCriteria(GetCriteriaFromOption(CmbOrderby.SelectedIndex), SearchBox.Text);
+            ProgressBarVisible(false);
+        }
+
+
+        private enum Criteria
+        {
+            Date, DateDesc, Length, LengthDesc, Rating, RatingDesc
+        }
+
+        private Criteria GetCriteriaFromOption(int option)
+        {
+            switch (option)
+            {
+                case 0: return Criteria.DateDesc;
+                case 1: return Criteria.Date;
+                case 2: return Criteria.LengthDesc;
+                case 3: return Criteria.Length;
+                case 4: return Criteria.RatingDesc;
+                case 5: return Criteria.Rating;
+                default: return Criteria.Date;
+            }
+        }
+
+        private async Task<List<Poetry>> GetPoetriesWithCriteria (Criteria criteria, string text)
+        {
+            switch(criteria)
+            {
+                case Criteria.Date:
+                    {
+                        return await App.MobileService.GetTable<Poetry>()
+                        .Where(p => p.UserId == UserHandler.Instance.GetUser().Id && (p.Title.ToLower().Contains(text) || p.Body.ToLower().Contains(text)))
+                        .OrderBy(poetry => poetry.CreationDate)
+                        .ToListAsync();
+                    }
+
+                case Criteria.DateDesc:
+                    {
+                        return await App.MobileService.GetTable<Poetry>()
+                        .Where(p => p.UserId == UserHandler.Instance.GetUser().Id && (p.Title.ToLower().Contains(text) || p.Body.ToLower().Contains(text)))
+                        .OrderByDescending(poetry => poetry.CreationDate)
+                        .ToListAsync();
+                    }
+
+                case Criteria.Length:
+                    {
+                        return await App.MobileService.GetTable<Poetry>()
+                        .Where(p => p.UserId == UserHandler.Instance.GetUser().Id && (p.Title.ToLower().Contains(text) || p.Body.ToLower().Contains(text)))
+                        .OrderBy(poetry => poetry.CharactersNumber)
+                        .ToListAsync();
+                    }
+
+                case Criteria.LengthDesc:
+                    {
+                        return await App.MobileService.GetTable<Poetry>()
+                        .Where(p => p.UserId == UserHandler.Instance.GetUser().Id && (p.Title.ToLower().Contains(text) || p.Body.ToLower().Contains(text)))
+                        .OrderByDescending(poetry => poetry.CharactersNumber)
+                        .ToListAsync();
+                    }
+                case Criteria.Rating:
+                    {
+                        return await App.MobileService.GetTable<Poetry>()
+                        .Where(p => p.UserId == UserHandler.Instance.GetUser().Id && (p.Title.ToLower().Contains(text) || p.Body.ToLower().Contains(text)))
+                        .OrderBy(poetry => poetry.Rating)
+                        .ToListAsync();
+                    }
+
+                case Criteria.RatingDesc:
+                    {
+                        return await App.MobileService.GetTable<Poetry>()
+                        .Where(p => p.UserId == UserHandler.Instance.GetUser().Id && (p.Title.ToLower().Contains(text) || p.Body.ToLower().Contains(text)))
+                        .OrderByDescending(poetry => poetry.Rating)
+                        .ToListAsync();
+                    }
+
+                default: return null;
+            }
         }
     }
 
